82f93797cf0fa0c44cc8cd6afa9a1ecf2c7d704c
Hjalti Sveinsson <erdmann123@gmail.com>
2014-10-30 18:42:16 +0000

    working on es getting all function in a bucket

--- js/api.js
+++ js/api.js
@@ -81,6 +81,10 @@ app.delete('/api/kodemon/delete', function(req, res){
 // #######################################
 // ###### Routes to elasticSearch ########
 
+
+
+
+
 /* Routes that we need to implement 
 	/api/es/kodemon/:project 
 	/api/es/kodemon/:project/:function
@@ -88,6 +92,63 @@ app.delete('/api/kodemon/delete', function(req, res){
 	/api/es/kodemon/:project/:function/time
 	/api/es/kodemon/:project/:function/time/:range
 													*/
+
+
+// Route: /api/es/kodemon/
+// Expected results:
+//		Returns a bucket list of all functions 
+app.get('/api/es/kodemon', function(req, res){
+	console.log('calling api/db/kodemon');
+	esClient.search({
+		index: "kodemon",
+		body: {
+    		"aggs" : {
+        		"keys" : {
+        			"range" : {
+	            		"field" : "timestamp",
+	            		"ranges" :[
+                    { "to" : "2013-06-02" },
+                    { "from" : "2013-06-02", "to" : "2014-10-29" },
+                    { "from" : "2014-10-29" }
+	            			  ]
+	            		
+        			}    				
+    			}
+
+			}
+		}
+		}).then(function(body) {
+			var keys = body.aggregations.keys.buckets;
+//			var keyresp = { "keys": keys };
+			console.log("HERE I AM !");
+			res.status(200).json(body);
+		}, function (err) {
+			res.status(error.status).send('Nothing found');
+		});
+});
+
+
+
+// þetta fall hér fyrir neðan verður hent ___
+app.get('/api/ess/kodemon', function(req, res){
+	console.log('calling api/db/kodemon');
+	esClient.search({
+		body: {
+    		"aggs" : {
+        		"keys" : {
+            		"terms" : { "field" : "key" }
+        			}
+    			}
+			}
+		}).then(function(body) {
+			var keys = body.aggregations.keys.buckets;
+			console.log("HERE I AM !");
+			res.status(200).json(body);
+		}, function (err) {
+			res.status(error.status).send('Nothing found');
+		});
+});
+
 
 //	Route: /api/es/kodemon/:project 
 //	Expected results: 	
