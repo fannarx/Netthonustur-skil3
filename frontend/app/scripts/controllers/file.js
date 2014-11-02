e790a220781de3699b7b917c2b637ffd7ed1bd86
fannarx <fannarms11@ru.is>
2014-11-02 02:42:34 +0000

    loads

--- frontend/app/scripts/controllers/file.js
+++ frontend/app/scripts/controllers/file.js
@@ -8,20 +8,154 @@
  * Controller of the frontendApp
  */
 angular.module('frontendApp')
-  .controller('FileCtrl', function ($scope, $http, $routeParams) {
-    $scope.file = $routeParams.file;
-    var url = 'http://localhost:5000/api/es/kodemon/'+ $scope.file;
+  .controller('FileCtrl', function ($scope, $http, $routeParams, $filter, $interval) {
+    /**PRETTY FEYH*/
+  $scope.file = $routeParams.file;
+  var url = 'http://localhost:5000/api/es/kodemon/'+ $scope.file;
+  $scope.loadingChart = true;  
+// Set all variables.
+    var sTime = new Date();
+      sTime.setDate(sTime.getDate()-3);
+      $filter('date')(sTime, 'yyyy-MM-dd:hh:mm:ss');
+    
+    var functionList = 
+         [{
+           'item':{'function': 'nan', 'data':[0], 'updateTime': sTime } 
+          }
+         ];
+
+    var getLatestData = $interval(function() { $scope.getLatestKodmonData(); }, 10000);
+    
+    $scope.getLatestKodmonData = function(){
+      for (var x in functionList){
+        console.log(functionList[x].item);
+        queryApi(functionList[x].item);
+      }
+    };
+
+    function queryApi(item){
+          $http.post('http://localhost:5000/api/es/kodemon/file/timerange', {
+            startTime: item.updateTime,
+            endTime: new Date(), 
+            fu: item.function
+          }).success( function  (data) {
+            // if there is no new data, we dont need to extract and resett timer.
+            if(data[0]){
+              extractChartData(data, item);
+              item.updateTime = new Date();
+              $filter('date')(item.updateTime, 'yyyy-MM-dd:hh:mm:ss');
+            }
+            console.log($scope.chartSeries);
+          });
+    }
+
+
+    function extractChartData(apiRespData,functionListItem){
+        for(var x in apiRespData){
+          functionListItem.data.push(apiRespData[x]._source.execution_time);
+        }
+    }
+
+    $scope.$on('$destroy', function(){
+        $interval.cancel(getLatestData);
+    });
+
+
+
+
+// ##### HIGHCHARTS EXPEREMENT
+
+
+  $scope.chartSeries = [];
+  $scope.chartTimeSeries = [];
+  
+  $scope.toggleHighCharts = function () {
+    this.chartConfig.useHighStocks = !this.chartConfig.useHighStocks;
+  };
+
+  $scope.chartConfig = {
+    options: {
+      chart: {
+        type: 'line'
+      },
+      plotOptions: {
+        series: {
+          stacking: ''
+        }
+      }
+    },
+    series: $scope.chartSeries,
+    title: {
+      text: 'Real time chart for ' + $routeParams.file
+    },
+    yAxis:{
+      labels:{
+        format: '{value} Nano sec'
+      }
+    },
+    credits: {
+      enabled: true
+    },
+    loading: true,
+    size: {}
+  };
+
+  $scope.chartTimeConfig = {
+    options: {
+      chart: {
+        type: 'line'
+      },
+      plotOptions: {
+        series: {
+          stacking: ''
+        }
+      }
+    },
+    series: $scope.chartTimeSeries,
+    title: {
+      text: 'Real time chart for ' + $routeParams.file
+    },
+    yAxis:{
+      labels:{
+        format: '{value} Nano sec'
+      }
+    },
+    credits: {
+      enabled: true
+    },
+    loading: false,
+    size: {}
+  };
+
+
+  $scope.reflow = function () {
+    $scope.$broadcast('highchartsng.reflow');
+  };
+
+
     //	Route: /api/es/kodemon/:project 
   	$http.get(url).success(function (data){
         $scope.functionByFileContainer = data.aggregations.group_by_functions.buckets;
-        console.log(data);
+        setupFunctionList($scope.functionByFileContainer);
+        $scope.chartConfig.loading = !$scope.chartConfig.loading;
+        $scope.selectedFunction = $scope.functionByFileContainer[0];
+        console.log($scope.functionByFileContainer[0].key);
       }).error(function(error){
         console.log(error);
       });
 
+    function setupFunctionList(data){
+      for (var i = data.length - 1; i >= 0; i--) {
+        var derp = {'item': { 'function': data[i].key, 'data':[0], 'updateTime': sTime, 'name': data[i].key }, 'id': data[i].key };
+        functionList.push(derp);
+        $scope.chartSeries.push(derp.item);
+
+      }
+    }
+
+
     $scope.getFunctionValuesByTimerange = function(i){
         //  Route: /api/es/kodemon/:project/:function/timerange
-        console.log(i);
         $http.post(url+'/timerange', {
           startTime: $scope.startDay,
           endTime: $scope.endDay, 
@@ -29,11 +163,20 @@ angular.module('frontendApp')
         }).success( function  (data) {
           console.log('POST: /kodemon/key.py/timerange');
           console.log(data);
+          var tmp = [];
+          for(var item in data){
+              tmp.push(data[item]._source.execution_time);
+          }
+          $scope.chartTimeSeries.push({'name': i.key ,'data':tmp, 'start': $scope.startDay});
           $scope.functionCallsFilteredByFunctionAndTimeContainer = data;
           console.log('RESP: /kodemon/key.py/timerange');
         });
     };
 
+$scope.removeTimeSeries = function(i){
+        var seriesArray = $scope.chartTimeSeries;
+        seriesArray.splice(i.id, 1);
+}
 
 $scope.sDay = function() {
     $scope.startDay = new Date();
