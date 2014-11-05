0f1823e1930aa6b0489d7239cef7eed326f6e1e7
Hjalti Sveinsson <erdmann123@gmail.com>
2014-11-05 00:15:40 +0000

    adding bash files and changed keyGame.py file

--- python/keyGame.py
+++ python/keyGame.py
@@ -5,26 +5,33 @@ import sys
 import os
 
 
+
 @kodemon
 def funny(key):
-	print '\aYou pressed: ' + key + ' not the right one'
-	os.system('say "you pressed the wrong key"')
+	print '\aYou pressed: ' + key + ' Only use letters not numbers'
+	if (sound):
+		os.system('say "you pressed the wrong key, use letters not numbers"')
 
 @kodemon
 def notFunny(key):
 	print 'Not ' + key + ' try again :p'
-	os.system('say "Not this one, try again"')
+	if (sound):
+		os.system('say "Not this one, try again"')
 
 @kodemon
 def winner():
-	os.system('say "ja, ja, halelujah"')
-	os.system('say "Tou erdt sigurvegari"')
+	if (sound):
+		os.system('say "Frabaert"')
+		os.system('say "Tou erdt sigurvegari"')
 
 @kodemon
 def mainFunction():
-	print 'In this game you have to guess which letter'
+	print 'Guess GAME\nSelect one of the following letters'
+	print 'If you select the right one you win one MILLION dollers'
 	print string.letters
 	rightKey = random.choice(string.letters)
+	if (showKey):
+		print rightKey
 	while True:
 		n = raw_input("Guess what letter to quit :) ")
 		i = n.strip()
@@ -36,6 +43,14 @@ def mainFunction():
 				funny(i)
 			else:
 				notFunny(i)
+
+showKey = False
+sound = True
+if (len(sys.argv) >= 2):
+	if (sys.argv[1] == 'nosound'):
+		sound = False
+	elif (sys.argv[1] == 'showkey'):
+		showKey = True
 
 
 mainFunction()
